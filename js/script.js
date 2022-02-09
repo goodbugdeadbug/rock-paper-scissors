0a454d964728c10def6f0e4c977db41600116bda
goodbugdeadbug <subprimepride@gmail.com>
2022-02-09 13:47:25 +1000

    Added player input

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
-let playerSelection = 'Rock';
-console.log(playerSelection);
-
-
-function computerPlay (playerSelection, computerSelection){
-    
-}
 // generate computerSelection
 const selectionArray = ['Rock','Paper','Scissors']
-let computerSelection = selectionArray[Math.floor(Math.random()*3)];
+let computerSelection = selectionArray[Math.floor(Math.random()*3)]; //const randomElement = array[Math.floor(Math.random() * array.length)];
 console.log(computerSelection);
 
-//const randomElement = array[Math.floor(Math.random() * array.length)];
+
 
 //request playerSelection
+let playerSelection = prompt("Type in your choise: Rock, Paper or Scissors. Input is case-sensitive");
+playerSelection = playerSelection.toLowerCase();
+if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
+    console.log('Good input!');
+}
+else {
+    console.log('Bad input!');
+}
+console.log(playerSelection);
+
+function computerPlay (playerSelection, computerSelection){
+}
+
 //run comparison for every combination
 //show result in console
