c9c6b8fb42eed62882711f010fb81a476a29f636
goodbugdeadbug <subprimepride@gmail.com>
2022-02-09 13:31:03 +1000

    proper js location and computerSelection function

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let playerSelection = 'Rock';
+console.log(playerSelection);
+
+
+function computerPlay (playerSelection, computerSelection){
+    
+}
+// generate computerSelection
+const selectionArray = ['Rock','Paper','Scissors']
+let computerSelection = selectionArray[Math.floor(Math.random()*3)];
+console.log(computerSelection);
+
+//const randomElement = array[Math.floor(Math.random() * array.length)];
+
+//request playerSelection
+//run comparison for every combination
+//show result in console
